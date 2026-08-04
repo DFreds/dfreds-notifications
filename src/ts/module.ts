51e623492d05901d9fa63bfa5597810fdfefe031
Derek Fredrickson <dfreds08@gmail.com>
2026-08-04 13:49:42 -0400

    Replace Foundry types with npm package

--- src/ts/module.ts
+++ src/ts/module.ts
@@ -1,3 +1,6 @@
+// Keep or else Foundry globals like game and Hooks will not resolve
+/// <reference types="@dfreds/foundry-types" />
+
 import "izitoast/dist/css/iziToast.min.css";
 import "../styles/style.scss"; // Keep or else vite will not include this
 import { HooksNotifications } from "./hooks/index.ts";
